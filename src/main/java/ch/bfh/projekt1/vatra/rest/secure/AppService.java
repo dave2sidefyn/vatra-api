c156b5d3b24c9f692333ecc5528cafc67a8f1cb1
dave <dave@sidefyn.ch>
2015-11-20 10:59:29 +0100

    Add Secured AppService

---
+++ src/main/java/ch/bfh/projekt1/vatra/rest/secure/AppService.java
@@ -0,0 +1,34 @@
+package ch.bfh.projekt1.vatra.rest.secure;
+
+
+import ch.bfh.projekt1.vatra.model.App;
+import ch.bfh.projekt1.vatra.rest.Greetings;
+import ch.bfh.projekt1.vatra.service.AppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest/secure/app")
+@RestController
+public class AppService {
+
+    @Autowired
+    private AppRepository appRepository;
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<App>> getApps() {
+        Iterable<App> all = appRepository.findAll();
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity postGreetings(@RequestBody Greetings greetings) {
+        System.out.println("Greetings have been SECURELY posted. They say: " + greetings.getGreetings());
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
