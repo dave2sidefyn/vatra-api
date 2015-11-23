cf3abc579b2351ce43afb0204f31b6f3afe8f8a9
dave <dave@sidefyn.ch>
2015-11-23 20:30:36 +0100

    Example for get Current User

--- src/main/java/ch/bfh/projekt1/vatra/rest/secure/AppService.java
+++ src/main/java/ch/bfh/projekt1/vatra/rest/secure/AppService.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @RequestMapping("/rest/secure/app")
 @RestController
 public class AppService {
@@ -22,6 +27,12 @@ public class AppService {
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Iterable<App>> getApps() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String name = authentication.getName();
+
+
         Iterable<App> all = appRepository.findAll();
         return new ResponseEntity<>(all, HttpStatus.OK);
     }
